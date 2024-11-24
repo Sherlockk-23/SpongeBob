757207851b2efb7ab3070fe765a9f523b07fec66
StevenZhou04 <145883468+StevenZhou04@users.noreply.github.com>
2024-11-24 10:30:08 +0800

    Changes to be committed: 	new file:   assets/pics/bubble.png 	new file:   assets/pics/gary_icon.png 	new file:   assets/pics/j_icon.png 	new file:   assets/pics/p_icon.jpg 	new file:   assets/pics/p_icon.png 	new file:   assets/pics/s_icon.jpg 	new file:   src/objects/characters/Patrick.ts

---
+++ src/objects/characters/Patrick.ts
@@ -0,0 +1,82 @@
+import * as THREE from "three";
+import { BaseCharacter } from "./BaseCharacter";
+import { GLTF } from 'three/examples/jsm/loaders/GLTFLoader';
+import { Renderer } from "../../scenes/Renderer";
+import { Scene } from "../../scenes/Scene";
+
+class Patrick extends BaseCharacter {
+    gltfDict: { [key: string]: GLTF };
+
+    constructor(name: string, characterGLTFDict: { [key: string]: GLTF }) {
+        let tmpgltfDict: { [key: string]: GLTF } = {};
+        tmpgltfDict['normal'] = characterGLTFDict['patrickAll'];
+        super(name, tmpgltfDict['normal']);
+        this.gltfDict = tmpgltfDict;
+        this.init();
+    }
+
+    updateConditionMesh(condition: string): { newgltf: GLTF } {
+        let newgltf: GLTF;
+        let animationId: number = 0;
+        switch (condition) {
+            case 'normal':
+                newgltf = this.gltfDict['normal'];
+                animationId = 0;
+                break;
+        }
+        return newgltf;
+
+    }
+    updateMovementAnimation(movement: string): { animationId: number; } {
+        let animationId: number = 0;
+        switch (movement) {
+            case 'idle':
+                if (this.condition == 'robotic') {
+                    animationId = 9;
+                } else if (this.condition == 'normal') {
+                    animationId = 13;
+                } else
+                    animationId = 12;
+                break;
+            case 'walking':
+                if (this.condition == 'robotic') {
+                    animationId = 9;
+                } else if (this.condition == 'normal') {
+                    animationId = 20;
+                } else
+                    animationId = 0;
+                break;
+            case 'running':
+                if (this.condition == 'robotic') {
+                    animationId = 9;
+                } else if (this.condition == 'normal') {
+                    animationId = 4;
+                } else
+                    animationId = 0;
+                break;
+            case 'jumping':
+                if (this.condition == 'robotic') {
+                    animationId = 7;
+                } else if (this.condition == 'normal') {
+                    animationId = 17;
+                } else
+                    animationId = 0;
+                break;
+            case 'punching':
+                if (this.condition == 'robotic') {
+                    animationId = 5;
+                } else if (this.condition == 'normal') {
+                    animationId = 23;
+                } else {
+                    animationId = 0;
+                }
+                break;
+            default:
+                animationId = 0;
+                break;
+        }
+        return { animationId: animationId };
+    }
+}
+
+export { Patrick };
